3e9ce4268fcefa309d2f43012e0f7fab6cffa760
lijealso <lijealso@gmail.com>
2025-05-20 21:50:26 +0100

    feat: implement answer selection and results screen navigation in Quiz widget

--- quizz_app/lib/quiz_64.dart
+++ quizz_app/lib/quiz_64.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/data/questions.dart';
 import 'package:quizz_app/start_screen.dart';
 import 'package:quizz_app/questions_screen.dart';
+import 'package:quizz_app/results_screen.dart';
 
 /// A widget that represents the Quiz application.
 /// This widget manages the state of the quiz and switches between the start screen and the questions screen.
@@ -13,6 +15,7 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   /// Doesn't require initState()
@@ -22,8 +25,28 @@ class _QuizState extends State<Quiz> {
     });
   }
 
+  void chosenAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+        // selectedAnswers.clear();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(onSelectAnswer: chosenAnswer);
+    }
+
+    if(activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(chosenAnswers: selectedAnswers);
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -37,9 +60,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ), // StartScreen widget is used here
       ),
     );
