822e3a15eb2af8435858fd4cfa97ebf47f32fcc0
lijealso <lijealso@gmail.com>
2025-05-23 20:14:19 +0100

    add onRestart, UI changes, navigation

--- quizz_app/lib/results_screen.dart
+++ quizz_app/lib/results_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_app/data/questions.dart';
 import 'questions_summary.dart';
+import 'quiz_64.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({super.key, required this.chosenAnswers, required this.onRestart,
+  });
 
   final List<String> chosenAnswers;
+
+  final void Function() onRestart;
 
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
@@ -16,9 +20,9 @@ class ResultsScreen extends StatelessWidget {
         'question': questions[i].questionText,
         'correct_answer': questions[i].answers[0],
         'user_answer': chosenAnswers[i],
+        'correct': questions[i].answers[0] == chosenAnswers[i],
       });
     }
-
     return summary;
   }
 
@@ -39,19 +43,30 @@ class ResultsScreen extends StatelessWidget {
           children: [
             Text(
               'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: const TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
             QuestionsSummary(getSummaryData()),
             const SizedBox(height: 30),
-            TextButton(
+            TextButton.icon(
+              label: const Text('Restart Quiz'),
+              icon: const Icon(Icons.refresh),
               style: TextButton.styleFrom(
                 foregroundColor: Colors.white,
-                textStyle: TextStyle(fontSize: 20),
+                textStyle: TextStyle(fontSize: 15),
               ),
               onPressed: () {
                 // Handle restart quiz action
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Quiz()),
+                );
               },
-              child: const Text('Restart Quiz'),
             ),
           ],
         ),
