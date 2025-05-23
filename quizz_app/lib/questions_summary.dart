fb13bf59278edb0c9e7883c887fdb5405c0df675
lijealso <lijealso@gmail.com>
2025-05-23 20:12:47 +0100

    formatting

--- quizz_app/lib/questions_summary.dart
+++ quizz_app/lib/questions_summary.dart
@@ -13,15 +13,58 @@ class QuestionsSummary extends StatelessWidget {
         child: Column(
           children: summaryData.map((data) {
             return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(((data['question_index'] as int) + 1).toString()),
+                Padding(
+                  padding: const EdgeInsets.only(right: 14.0),
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor:
+                        data['user_answer'] == data['correct_answer']
+                        ? Colors.green
+                        : Colors.red,
+                    child: Text(
+                      ((data['question_index'] as int) + 1).toString(),
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
                 Expanded(
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment
+                        .start, // This ensures left alignment.
                     children: [
-                      Text(data['question'] as String),
-                      SizedBox(height: 10),
-                      Text(data['user_answer'] as String),
-                      Text(data['correct_answer'] as String),
+                      Text(
+                        data['question'] as String,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        data['user_answer'] as String,
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 69, 8, 126),
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        data['correct_answer'] as String,
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
                     ],
                   ),
                 ),
