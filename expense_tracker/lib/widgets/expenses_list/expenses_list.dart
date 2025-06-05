bd8b76d6afdddf2ba96bc31568a19fc486b98496
lijealso <lijealso@gmail.com>
2025-06-05 22:33:41 +0100

    list of expenses

---
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../../models/expense.dart';
+import '../../widgets/expenses_list/expense_item.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({super.key, required this.expenses, required this.onRemoveExpense});
+
+  final void Function(Expense expense) onRemoveExpense;
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        child: ExpenseItem(expenses[index]))
+    );
+  }
+}
